66c160b1372a7dc9d15f28cb9622d869186d3f23
suchan <scdreamricher@gmail.com>
2022-03-28 13:55:46 +0900

    create a redux thunk middleware.

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import dictionary from "./modules/dictionary";
+import thunk from "redux-thunk";
 
+const milddlewares = [thunk];
 const rootReducer = combineReducers({ dictionary });
+const enhancer = applyMiddleware(...milddlewares);
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancer);
 
 export default store;
